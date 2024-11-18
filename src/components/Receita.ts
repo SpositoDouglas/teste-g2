c4852ea517ac138d85694951cbaef12d2e3ea351
Douglas <douglas_sposito2015@outlook.com>
2024-11-18 20:01:20 -0300

    criação do Receita.ts

---
+++ src/components/Receita.ts
@@ -0,0 +1,10 @@
+export interface Receita {
+    id: string;
+    nome: string;
+    tipo: string;
+    qtd_pessoas: number;
+    dificuldade: string;
+    ingredientes: string[];
+    etapas: number;
+  }
+  
